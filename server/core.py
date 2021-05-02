4ec8cc99a35b4b7845aeef290eac41a72a2428ee
link857 <2761766788@qq.com>
2021-05-02 14:38:59 +0800

    copy down the exegesis of 'core' in 'server-main'

--- server/core.py
+++ server/core.py
@@ -8,9 +8,14 @@ from server.typing_ import CrutchStatus, Loc, CrutchSettings
 
 
 class Crutch(object):
+
+    # If the time delta between crutch's last heartbeat and now exceeds the threshold,
+    # judge the crutch is offline.
+    # ** THE THRESHOLD SHOULD BE LONGER THAN THE HEARTBEAT INTERVAL SET ON THE DEMOBOARD **
     OFFLINE_TIME_THRESHOLD: float = 8.0
 
     def __eq__(self, other):
+        # For finding crutch obj with uuid or username
         return self.uuid == other.uuid or (self.username and self.username == other.username)
 
     def __init__(self, uuid: Optional[str] = None,
@@ -29,13 +34,9 @@ class Crutch(object):
 
         self._settings = CrutchSettings()
 
-        # self._image: Optional[image]
-
         if settings:
             self._settings = CrutchSettings.parse_raw(settings)
 
-        # if image:
-        #     self._image = CrutchImage.parse_raw(image)
     @property
     def settings(self) -> CrutchSettings:
         return self._settings
@@ -57,7 +58,11 @@ class Crutch(object):
     @property
     def status(self) -> CrutchStatus:
         if self._status == CrutchStatus.ok and time() - self._last_conn_time > self.OFFLINE_TIME_THRESHOLD:
+
+            # The time delta between crutch's last connection and now
+            # exceeds OFFLINE_TIME_THRESHOLD, return Offline status
             return CrutchStatus.offline
+
         return self._status
 
     @status.setter
@@ -65,29 +70,30 @@ class Crutch(object):
         self._last_conn_time = time()
         self._status = status
 
-    # @property
-    # def image(self) -> CrutchImage:
-    #     if self._img == CrutchImage.ok and time() - self._last_conn_time > self.OFFLINE_TIME_THRESHOLD:
-    #         return CrutchImg.offline
-    #     return self._img
 
-    # @image.setter
-    # def image(self, status: CrutchImage):
-    #     self._last_conn_time = time()
-    #     self._image = image
-
-
+# Crutch obj list, the crutch instances are maintained in the list while running
 crutch_obj_list: List[Crutch]
+
+# database instance
 db: DataBase
 
 
 def load_database():
+    """
+    **INTERNAL FUNCTION**
+    Load all crutch obj from database. Being called automatically when server initializing.
+    """
     global crutch_obj_list, db
     db = DataBase()
     crutch_obj_list = [Crutch(uuid, username, settings) for uuid, username, settings in db.read_all()]
 
 
 def get_crutch_uuid(username: str) -> Optional[str]:
+    """
+    Get a crutch uuid with username. Mainly used for app login.
+    :param username: the username crutch bound
+    :return: uuid if the crutch is found, otherwise return None
+    """
     try:
         idx = crutch_obj_list.index(Crutch(username=username))
     except ValueError:
@@ -97,6 +103,11 @@ def get_crutch_uuid(username: str) -> Optional[str]:
 
 
 def get_crutch_obj(uuid: str) -> Optional[Crutch]:
+    """
+    Get a registered crutch obj with uuid.
+    :param uuid: cruch UUID
+    :return: crutch obj if the obj exist, otherwise return None
+    """
     try:
         idx = crutch_obj_list.index(Crutch(uuid=uuid))
     except ValueError:
