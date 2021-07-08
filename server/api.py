fcaf80a81c0bff55267efede25a5fdc010a55a02
link857 <2761766788@qq.com>
2021-07-08 20:29:18 +0800

    return to location info

--- server/api.py
+++ server/api.py
@@ -56,6 +56,8 @@ def heartbeat(data: HeartbeatIn):
     - loc: *可选项*，拐杖位置信息
         - latitude: 纬度
         - longitude: 经度
+        - info: 位置描述
+
 
     #### Response
     - code: 返回值:
@@ -377,6 +379,7 @@ def get_loc(uuid: str):
     - loc: *可选项*，拐杖位置信息
         - latitude: 纬度
         - longitude: 经度
+        - info: 位置描述
 
     """
 
